418b8c3b19ae45a462d5a57849ad121fcf9a25fe
Kevin Miller <keveemiller@gmail.com>
2020-06-15 06:02:34 -0600

    chore: React playsinline

--- src/App.js
+++ src/App.js
@@ -87,7 +87,7 @@ const App = () => {
           </button>
         </div>
       )}
-      <video src="/boop.mp4" ref={videoRef} playsinline />
+      <video src="/boop.mp4" ref={videoRef} playsInline />
     </div>
   )
 }
